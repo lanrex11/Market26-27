415bdcdb663c2a5a89f65847a96a7ea1bcd0be5f
lanrex11 <lanrex11@rog-strix-g15>
2026-09-26 10:36:47 +0200

    Consistency (low eta high) issuak konpondu

--- src/main/java/businessLogic/BLFacadeImplementation.java
+++ src/main/java/businessLogic/BLFacadeImplementation.java
@@ -47,7 +47,7 @@ import java.io.IOException;
 public class BLFacadeImplementation  implements BLFacade {
 	private static final int baseSize = 160;
 
-	private static final String basePath="src/main/resources/images/";
+	private static final String BASE_PATH="src/main/resources/images/";
 	DataAccess dbManager;
 
 	public BLFacadeImplementation()  {		
@@ -229,7 +229,7 @@ public class BLFacadeImplementation  implements BLFacade {
 	 * {@inheritDoc}
 	 */
 	@WebMethod public Image downloadImage(String imageName) {
-		File image = new File(basePath+imageName);
+		File image = new File(BASE_PATH+imageName);
 		try {
 			return ImageIO.read(image);
 		} catch (IOException e) {
